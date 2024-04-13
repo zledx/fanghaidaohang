d1e1a6e112cc3683b796907d0dded77f5e86a696
zledx <166598650+zledx@users.noreply.github.com>
2024-04-13 22:34:54 +0800

    components/Common/Footer.vue

--- components/Common/Footer.vue
+++ components/Common/Footer.vue
@@ -1,47 +1,20 @@
 <template>
-<style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        h2 {
-            font-size: 16px;
-            margin-bottom: 10px;
-        }
-        ul {
-            list-style-type: none;
-            padding: 0;
-            margin: 0;
-        }
-        li {
-            display: inline;
-            margin-right: 16px;
-        }
-        a {
-            text-decoration: none;
-            color: #333;
-            font-size: 16px;
-        }
-
-        /* 响应式设计 */
-        @media only screen and (max-width: 600px) {
-            h2 {
-                font-size: 16px;
-            }
-            li {
-                display: block;
-                margin-bottom: 10px;
-            }
-        }
-    </style>
-    <h2>友情链接</h2>
-    <ul>
-        <li><a href="https://example.com">友情链接 1</a></li>
-        <li><a href="https://example.net">友情链接 2</a></li>
-        <li><a href="https://example.org">友情链接 3</a></li>
-    </ul>
 	<footer class="text-left text-[#6c757d] p-6 bg-[rgba(248,249,250,1)] h-[70px]">
 	
 		<section class="flex justify-center">
+		<div class="text-[14px] h-[30px]">
+				<span
+					>友情链接： 
+					
+					<a
+						href="https://fanghai.site"
+						target="_blank"
+						class="decoration-transparent text-[#000]"
+						>FANGHAI</a
+					>
+				</span>
+				
+			</div>
 			<!-- Left -->
 			<div class="text-[14px] h-[30px]">
 				<span
